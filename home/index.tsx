b332c457f6a2b018bc0e653b0a1339124b95babf
KimDongwu <hugo@alphaworks.xyz>
2022-06-20 16:59:05 +0900

    setting

---
+++ home/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Next template</title>
+        <meta name="description" content="Next template" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+};
+
+export default Home;
